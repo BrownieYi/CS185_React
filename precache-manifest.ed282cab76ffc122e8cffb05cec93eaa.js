6fd9b910368e5569203e22029067b1418494a03a
Mingyu Xie <mingyuxie@ucsb.edu>
2020-04-26 22:21:51 +0800

    Updates

---
+++ precache-manifest.ed282cab76ffc122e8cffb05cec93eaa.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e65e6d95acb254fc54d675838f0b452",
+    "url": "/CS185_React/index.html"
+  },
+  {
+    "revision": "3db8dc6447924b264943",
+    "url": "/CS185_React/static/css/main.4a58dfe7.chunk.css"
+  },
+  {
+    "revision": "383c3b3952253808dbf0",
+    "url": "/CS185_React/static/js/2.32037b86.chunk.js"
+  },
+  {
+    "revision": "6e742f513d9938d07b2d93218f3e4449",
+    "url": "/CS185_React/static/js/2.32037b86.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3db8dc6447924b264943",
+    "url": "/CS185_React/static/js/main.c9bd7d19.chunk.js"
+  },
+  {
+    "revision": "3200cc20c901e17dd909",
+    "url": "/CS185_React/static/js/runtime-main.12e81fe5.js"
+  },
+  {
+    "revision": "9125cc8a78fbf9b9955ab39f9420dd42",
+    "url": "/CS185_React/static/media/10.9125cc8a.jpg"
+  },
+  {
+    "revision": "760c38a0e64626c47fdf674d6589455d",
+    "url": "/CS185_React/static/media/2.760c38a0.jpg"
+  },
+  {
+    "revision": "8ed8b620fb8984c86f92d2dc913d35da",
+    "url": "/CS185_React/static/media/4.8ed8b620.jpg"
+  },
+  {
+    "revision": "a70a3f9753a0a934cccbac566735ca0d",
+    "url": "/CS185_React/static/media/5.a70a3f97.jpg"
+  },
+  {
+    "revision": "032d8a50ba5f97079b804a6f6043bd56",
+    "url": "/CS185_React/static/media/8.032d8a50.jpg"
+  },
+  {
+    "revision": "2563c3ccadcf71fce6a14aacfb6168cd",
+    "url": "/CS185_React/static/media/9.2563c3cc.jpg"
+  },
+  {
+    "revision": "8f3e2763eb025764fa21fa95ae5f93e2",
+    "url": "/CS185_React/static/media/IMG_2083.8f3e2763.jpg"
+  },
+  {
+    "revision": "189c35b938448892bc1f1316265423bf",
+    "url": "/CS185_React/static/media/IMG_2120.189c35b9.jpg"
+  },
+  {
+    "revision": "5e8380cffca99496cb32625c61d822ea",
+    "url": "/CS185_React/static/media/IMG_2146.5e8380cf.jpg"
+  },
+  {
+    "revision": "fc9d794def37e0ce490f967760b9b6e9",
+    "url": "/CS185_React/static/media/IMG_2152.fc9d794d.jpg"
+  },
+  {
+    "revision": "65469962118b72f2f553a17f18fcc660",
+    "url": "/CS185_React/static/media/league.65469962.jpg"
+  },
+  {
+    "revision": "e315124802e2cebb91b9298ba16a590e",
+    "url": "/CS185_React/static/media/mhw.e3151248.jpg"
+  },
+  {
+    "revision": "f6b2a1002a6d252f756053ae0f1088fc",
+    "url": "/CS185_React/static/media/sekiro.f6b2a100.jpg"
+  }
+]);
